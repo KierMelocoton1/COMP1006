34f95534c582d4eefa10a829b29dcd969951d708
KierMelocoton <Kier.Melocoton@MyGeorgian.ca>
2026-01-15 23:58:06 -0500

    Comment

--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -3,3 +3,5 @@ require "header.php";
 echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
 require "footer.php"; 
 require "car.php";
+
+// I found the local server and creating the database the most difficult, so much so that I am not sure I even did it properly.
