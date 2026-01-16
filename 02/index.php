d3db7e5de71642c00c9b700525d04d28b513769d
KierMelocoton <Kier.Melocoton@MyGeorgian.ca>
2026-01-15 23:36:35 -0500

    commit

--- 02/index.php
+++ 02/index.php
@@ -3,12 +3,38 @@
 
 //2. Code Commenting 
 
+// inline comment
+
+<?php
+declare (strict_types=1);
 
 //3. Variables, Data Tyoes, Concatenation, Conditional Statements & Echo
 
+$firstName = "Kier"; // string
+$lastName = "Melocoton"; // string
+$age = 23; //int
+$isInstructor = true; //boolean;
+
+echo "<p> Hello there, my name is" . $firstName . "" . $lastNme . "</p>";
+
+if ($isInstructor) {
+    echo "<p> I am your teacher <p>";
+{
+    else 
+}
+    echo "<p> Oops, the teacher didn't show! </p>";
+}
 
 //4. Loosely Typed Language Demo
 
+$num1 = 1;
+$num2 = "10";
+
+function add(int $num1, int $num2) : int {
+    return $num1 + $num2;
+}
+
+echo "<p>" . add($num1, $num2) . "</p>";
 
 //5. Strict Types & Types Hints
 
---
+++ assignments/labs/lab_one/car.php
@@ -0,0 +1,23 @@
+<?php
+class car {
+
+    //PROPERTIES
+    $maker = Porsche; //string
+    $model = Carrera ; //string
+    $year = 2025; //int
+
+    //METHODS
+    function add(string $maker, string $model, int $year);
+    
+    //OUTPUT
+    echo "<p> A " . $maker ." ". $model ." ". $year ." ". "is a great car!" . "</p>";
+
+    //INSTANCE
+    if ($otherCarBrand) {
+        echo "<p> This is a another type of car"
+    {
+        else
+    }
+        echo "<p> This car brand is unknown"
+    }
+}
