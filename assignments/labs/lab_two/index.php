3af0de8201adc1ef1852cbbd8d156c1ff88deed1
KierMelocoton <Kier.Melocoton@MyGeorgian.ca>
2026-01-22 23:39:32 -0500

    commit

---
+++ assignments/labs/lab_two/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Page</title>
+</head>
+
+<body>
+    <h1>Welcome</h1>
+    <ul>
+        <?php
+        /* What's the Problem? 
+            - PHP logic + HTML in one file
+            - Works, but not scalable
+            - Repetition will become a problem
+
+            How can we refactor this code so it’s easier to maintain?
+        */
+
+        $items = ["Home", "About", "Contact"];
+        foreach ($items as $item):
+            echo "<li> $item";
+        endforeach;
+        ?>
+    </ul>
+
+<footer>
+    <p>&copy; 2026</p>
+</footer>
+
+</body>
+</html>
